daa5f0b28258d6b213a743a7e37bc9bfddb18b01
vladislav <vlad.matyunin96@gmail.com>
2017-07-09 16:33:54 +0300

    initial commit, listview added

---
+++ app/src/main/java/com/example/vladislav/listviewtest/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.vladislav.listviewtest;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends ListActivity {
+
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.my_list_item,R.id.label, values);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String item = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, item + " выбран", Toast.LENGTH_LONG).show();
+    }
+}
